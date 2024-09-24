7700b8a1a0c4e4ea2798355bbd6d8ca3a8f6700c
gaga999 <jjjj4444.h@gmail.com>
2024-09-24 16:07:49 +0800

    ac automaton test

---
+++ String/ACautomaton.cpp
@@ -0,0 +1,8 @@
+struct ACauto {
+    const static int N = 2e5 + 5;
+    int tr[26][N], fail[N], ctn = 0, cnt[N], endat[N];
+    inline void add(const string &s) {
+        }
+    void blt(const vector<sring> &ar) {
+    }
+};
