6b3ff03eba3a298888d207c95b07e2a9bfbe6fea
qwe1rt1yuiop1 <ynnek0130@gmail.com>
2024-09-24 23:55:19 +0800

    Update FastWalshTransform.cpp

--- Math/FastWalshTransform.cpp
+++ Math/FastWalshTransform.cpp
@@ -19,11 +19,8 @@ struct Fast_Walsh_Transform {
                         v[j ^ (1 << i)] = (x + y) * iv2, v[j] = (x - y) * iv2;
                 }
     }
-    vector<mint> v1, v2;
-    Fast_Walsh_Transform(vector<mint> _v1, vector<mint> _v2, string _op) : v1(_v1), v2(_v2), op(_op) {
-        assert(size(v1) == size(v2));
-        assert((1 << __lg(size(v1))) == size(v1));
-    }
+    vector<mint> v1, v2; // size(v1) = size(v2) = 2^k
+    Fast_Walsh_Transform(const vector<mint> &_v1, const vector<mint> &_v2, const string &_op) : v1(_v1), v2(_v2), op(_op) {}
     vector<mint> solve() {
         fwt(v1, 0), fwt(v2, 0);
         for (int i = 0; i < size(v1); ++i)
