7e9c72b44a69f1e9833196433073ddbf34b0c551
gaga999 <jjjj4444.h@gmail.com>
2024-09-24 15:24:56 +0800

    ttwd

--- Graph/DominaterTree.cpp
+++ Graph/DominaterTree.cpp
@@ -1,17 +1,14 @@
-const int N = 2e5 + 5;
-struct DOT
-{
+struct DOT {
+    static const int N = 2e5 + 5;
     int dfn[N], id[N], dfc, fa[N], idm[N], sdm[N], bst[N];
     vector<int> G[N], rG[N];
-    void ini(int n)
-    {
+    void ini(int n) {
         for (int i = 1; i <= n; i++)
             G[i].clear(), rG[i].clear();
         fill(dfn, dfn + n + 1, 0);
     }
     inline void adeg(int u, int v) { G[u].pb(v), rG[v].pb(u); }
-    int f(int x, int lm)
-    {
+    int f(int x, int lm) {
         if (x <= lm)
             return x;
         int cr = f(fa[x], lm);
@@ -19,31 +16,27 @@ struct DOT
             bst[x] = bst[fa[x]];
         return fa[x] = cr;
     }
-    void dfs(int u)
-    {
+    void dfs(int u) {
         id[dfn[u] = ++dfc] = u;
         for (int v : G[u])
             if (!dfn[v])
                 dfs(v), fa[dfn[v]] = dfn[u];
     }
-    void tar(vector<int> *eg, int rt)
-    {
+    void tar(vector<int> *eg, int rt) {
         dfc = 0, dfs(rt);
         for (int i = 1; i <= dfc; i++)
             sdm[i] = bst[i] = i;
-        for (int i = dfc; i > 1; i--)
-        {
+        for (int i = dfc; i > 1; i--) {
             int u = id[i];
             for (int v : rG[u])
                 if ((v = dfn[v]))
-                    f(v, i), sdm[i] = (sdm[i]>sdm[bst[v]])?(sdm[bst[v]]):(sdm[i]);
+                    f(v, i), sdm[i] = (sdm[i] > sdm[bst[v]]) ? (sdm[bst[v]]) : (sdm[i]);
             eg[sdm[i]].pb(i), u = fa[i];
             for (int v : eg[u])
                 f(v, u), idm[v] = (sdm[bst[v]] == u ? u : bst[v]);
             eg[u].clear();
         }
-        for (int i = 2; i <= dfc; i++)
-        {
+        for (int i = 2; i <= dfc; i++) {
             if (sdm[i] != idm[i])
                 idm[i] = idm[idm[i]];
             eg[id[idm[i]]].pb(id[i]);
