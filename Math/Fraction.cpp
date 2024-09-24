4a4ee15b65292f0bb5a20f4eb214a87d60b082f2
qwe1rt1yuiop1 <ynnek0130@gmail.com>
2024-09-24 14:38:36 +0800

    Update Fraction.cpp

--- Math/Fraction.cpp
+++ Math/Fraction.cpp
@@ -1,37 +1,29 @@
-struct frac
-{
-	ll n, d;
-	frac(const ll &_n = 0, const ll &_d = 1) : n(_n), d(_d)
-	{
-		ll t = __gcd(n, d);
-		n /= t, d /= t;
-		if (d < 0)
-			n = -n, d = -d;
-	}
-	frac operator-() const
-	{
-		return frac(-n, d);
-	}
-	frac operator+(const frac &b) const
-	{
-		return frac(n * b.d + b.n * d, d * b.d);
-	}
-	frac operator-(const frac &b) const
-	{
-		return frac(n * b.d - b.n * d, d * b.d);
-	}
-	frac operator*(const frac &b) const
-	{
-		return frac(n * b.n, d * b.d);
-	}
-	frac operator/(const frac &b) const
-	{
-		return frac(n * b.d, d * b.n);
-	}
-	void print()
-	{
-		cout << n;
-		if (d != 1)
-			cout << "/" << d;
-	}
+struct frac {
+    ll n, d;
+    frac(const ll &_n = 0, const ll &_d = 1) : n(_n), d(_d) {
+        ll t = __gcd(n, d);
+        n /= t, d /= t;
+        if (d < 0)
+            n = -n, d = -d;
+    }
+    frac operator-() const {
+        return frac(-n, d);
+    }
+    frac operator+(const frac &b) const {
+        return frac(n * b.d + b.n * d, d * b.d);
+    }
+    frac operator-(const frac &b) const {
+        return frac(n * b.d - b.n * d, d * b.d);
+    }
+    frac operator*(const frac &b) const {
+        return frac(n * b.n, d * b.d);
+    }
+    frac operator/(const frac &b) const {
+        return frac(n * b.d, d * b.n);
+    }
+    friend auto operator<<(ostream &os, frac const &f) -> ostream & {
+        if (f.d == 1)
+            return os << f.n;
+        return os << f.n << '/' << f.d;
+    }
 };
