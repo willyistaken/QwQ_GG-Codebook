424c2c2850fc78019368e3a6281b2e27117cc07f
gaga999 <jjjj4444.h@gmail.com>
2024-10-10 16:17:08 +0800

    geometry

--- Geometry/GeometryTemplate.cpp
+++ Geometry/GeometryTemplate.cpp
@@ -75,12 +75,13 @@ void fdhl(vector<P> &ar, vector<P> &hl) {
     if (size(hl) > 1 && hl.front() == hl.back())
         hl.pop_back();
 }
-bool in(const P &a, vector<P> &hl) {
+bool in(const P &a, const vector<P> &hl) {
     int ln = size(hl);
     if (ln == 1)
         return a == hl[0];
-    if (ln == 2)
-        return within(a, hl[0], hl[1]);
+    if (ln == 2) {
+        return !ori(hl[0], hl[1], a) && within(a, hl[0], hl[1]);
+    }
     int l = 1, r = ln - 1, m;
     while (l < r - 1) {
         m = (l + r) >> 1;
