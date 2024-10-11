6c2dbf32cad246f4dd20c08c0e90fc6a331287bf
gaga999 <jjjj4444.h@gmail.com>
2024-10-11 13:45:18 +0800

    modintfatser

--- Math/ModInt.cpp
+++ Math/ModInt.cpp
@@ -1,47 +1,46 @@
 template <unsigned P>
 struct mint { // P not prime break /=
     unsigned v;
-    mint(ll v = 0) : v(v % P) {}
-    mint &operator+=(mint const &o) {
+    constexpr mint(ll v = 0) : v(v % P) {}
+    constexpr mint &operator+=(mint const &o) {
         v = (v += o.v) >= P ? v - P : v;
         return *this;
     }
-    mint &operator-=(mint const &o) {
+    constexpr mint &operator-=(mint const &o) {
         v = (v < o.v) ? v + P - o.v : v - o.v;
         return *this;
     }
-    mint &operator*=(mint const &o) {
-        ll ret = ll(v) * ll(o.v) - P * ll(1.L / P * ll(v) * ll(o.v));
-        v = ret + P * (ret < 0) - P * (ret >= (ll)P);
+    constexpr mint &operator*=(mint const &o) {
+        v = 1ll * v * o.v % P;
         return *this;
     }
-    friend mint operator+(mint const &a, mint const &b) {
+    constexpr friend mint operator+(mint const &a, mint const &b) {
         return mint(a) += b;
     }
-    friend mint operator-(mint const &a, mint const &b) {
+    constexpr friend mint operator-(mint const &a, mint const &b) {
         return mint(a) -= b;
     }
-    friend mint operator*(mint const &a, mint const &b) {
+    constexpr friend mint operator*(mint const &a, mint const &b) {
         return mint(a) *= b;
     }
-    mint pow(ll n) const {
+    constexpr inline mint pow(ll n) const {
         mint r(1);
         mint a = v;
         for (; n; a *= a, n >>= 1)
             r *= (n & 1) ? (a) : (mint(1));
         return r;
     }
-    mint &operator/=(mint const &o) {
+    constexpr mint &operator/=(mint const &o) {
         *this *= o.pow(P - 2);
         return *this;
     }
-    friend mint operator/(mint const &a, mint const &b) {
+    constexpr friend mint operator/(mint const &a, mint const &b) {
         return mint(a) /= b;
     }
-    friend ostream &operator<<(ostream &os, mint const &m) {
+    constexpr friend ostream &operator<<(ostream &os, mint const &m) {
         return os << m.v;
     }
-    friend istream &operator>>(istream &is, mint &m) {
+    constexpr friend istream &operator>>(istream &is, mint &m) {
         return is >> m.v;
     }
 };
