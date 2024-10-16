485c878cf8bc16c48e156f92f530fae3b7b72611
qwe1rt1yuiop1 <ynnek0130@gmail.com>
2024-10-16 15:08:35 +0800

    Update VertexBCC.cpp

--- Graph/VertexBCC.cpp
+++ Graph/VertexBCC.cpp
@@ -1,112 +1,59 @@
-struct BCC
-{
-	int n, dft, nbcc;
-	vector<pii> Edges;
-	vector<int> low, dfn, bln, stk, is_ap, bln_, BCCofE, bct_vst;
+struct VertexBCC { // !simple affect BCCofE
+	int n, m, dft;
+	vector<int> low, dfn, bln, is_ap, st1, st2, BCCofE;
+	vector<pii> E;
 	vector<vector<pii>> G;
-	vector<vector<int>> bcc, nG;
-	void make_bcc(int u)
-	{
-		bcc.emplace_back(1, u);
-		for (; stk.back() != u; stk.pop_back())
-			bln[stk.back()] = nbcc, bcc[nbcc].emplace_back(stk.back());
-		stk.pop_back(), bln[u] = nbcc++;
-	}
-	void dfs(int u, int f)
-	{
+	vector<vector<int>> nG, bcc;
+	void dfs(int u, int f) {
 		int child = 0;
-		low[u] = dfn[u] = ++dft, stk.emplace_back(u);
+		low[u] = dfn[u] = ++dft, st1.pb(u);
 		for (auto [v, id] : G[u])
-			if (!dfn[v])
-			{
+			if (!dfn[v]) {
+				st2.pb(id);
 				dfs(v, u), ++child;
-				low[u] = min(low[u], low[v]);
-				if (dfn[u] <= low[v])
-				{
-					is_ap[u] = 1, bln[u] = nbcc;
-					make_bcc(v), bcc.back().emplace_back(u);
+				tmin(low[u], low[v]);
+				if (dfn[u] <= low[v]) {
+					is_ap[u] = 1, bln[u] = bln[v] = size(bcc);
+					bcc.pb(vector<int>(1, u)), bcc.back().pb(v);
+					for (; st1.back() != v; st1.pop_back())
+						bcc.back().pb(st1.back()), bln[st1.back()] = bln[u];
+					st1.pop_back();
+					while (st2.back() != id)
+						BCCofE[st2.back()] = bln[u], st2.pop_back();
+					BCCofE[id] = bln[u], st2.pop_back();
 				}
 			}
-			else if (dfn[v] < dfn[u] && v != f)
-				low[u] = min(low[u], dfn[v]);
+			else if (v != f) {
+				tmin(low[u], dfn[v]);
+				if (dfn[v] < dfn[u])
+					st2.pb(id);
+			}
 		if (f == -1 && child < 2)
 			is_ap[u] = 0;
 		if (f == -1 && child == 0)
-			make_bcc(u);
+			bcc.pb(vector<int>(1, u));
 	}
-	BCC(int _n) : n(_n), dft(0), nbcc(0), low(n), dfn(n), bln(n), is_ap(n), G(n) {}
-	void solve()
-	{
-		for (int i = 0; i < size(Edges); ++i)
-		{
-			G[Edges[i].first].emplace_back(Edges[i].second, i);
-			G[Edges[i].second].emplace_back(Edges[i].first, i);
-		}
+	VertexBCC(int _n) : n(_n), m(0), E(0), low(n), dfn(n), bln(n), G(n) {}
+	void add_edge(int u, int v) {
+		E.pb(u, v), G[u].pb(v, m), G[v].pb(u, m++);
+	}
+	void slv() {
+		is_ap.assign(n, 0), dfn = is_ap;
+		dft = 0, st1.clear(), st2.clear();
+		BCCofE.assign(m, -1);
 		for (int i = 0; i < n; ++i)
 			if (!dfn[i])
 				dfs(i, -1);
 	}
-	void block_cut_tree()
-	{
-		int tmp = nbcc;
+	void block_cut_tree() {
+		int tmp = size(bcc);
 		for (int i = 0; i < n; ++i)
 			if (is_ap[i])
 				bln[i] = tmp++;
-		bln_.resize(tmp);
-		for (int i = 0; i < n; ++i)
-			bln_[bln[i]] = i;
 		nG.assign(tmp, vector<int>(0));
-		for (int i = 0; i < nbcc; ++i)
+		for (int i = 0; i < size(bcc); ++i)
 			for (int j : bcc[i])
 				if (is_ap[j])
-					nG[i].emplace_back(bln[j]), nG[bln[j]].emplace_back(i);
+					nG[i].pb(bln[j]), nG[bln[j]].pb(i);
 	} // up to 2 * n - 2 nodes!! bln[i] for id
-	void dfs_bct(int cur, int fa)
-	{
-		if (cur < nbcc)
-			bct_vst[cur] = 1;
-		for (auto i : nG[cur])
-		{
-			if (i == fa)
-				continue;
-			if (cur < nbcc)
-			{
-				for (auto [j, id] : G[bln_[i]])
-				{
-					auto it = lower_bound(iter(bcc[cur]), j);
-					if (it != bcc[cur].end() && *it == j)
-						BCCofE[id] = cur;
-				}
-			}
-			else
-			{
-				for (int j : bcc[i])
-				{
-					auto it = lower_bound(iter(G[bln_[cur]]), make_pair(j, 0));
-					if (it != G[bln_[cur]].end() && it->first == j)
-						BCCofE[it->second] = i;
-				}
-			}
-			dfs_bct(i, cur);
-		}
-	}
-	void find_BCCofE()
-	{
-		for (auto &i : G)
-			sort(iter(i));
-		for (auto &i : bcc)
-			sort(iter(i));
-		BCCofE.assign(size(Edges), -1), bct_vst.assign(nbcc, 0);
-		for (int i = 0; i < nbcc; ++i)
-			if (!bct_vst[i])
-				dfs_bct(i, i);
-		for (int i = 0; i < size(Edges); ++i)
-			if (is_ap[Edges[i].first] || is_ap[Edges[i].second])
-				assert(BCCofE[i] != -1);
-			else
-			{
-				assert(BCCofE[i] == -1);
-				BCCofE[i] = bln[Edges[i].first];
-			}
-	}
 };
