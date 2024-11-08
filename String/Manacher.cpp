78e099087b7bcecfcb78113d810371728a50a32c
willyistaken <willyhlchan@gmail.com>
2024-11-08 14:44:21 +0800

    update manacher

--- String/Manacher.cpp
+++ String/Manacher.cpp
@@ -1,15 +1,13 @@
-vector<int> manacher(string &ss) { // biggest k such [i-k+1,i]=[i~i+k-1],padded with $
-    string s;
-    s.resize(size(ss) * 2 + 1, '$');
-    for (int i = 0; i < size(ss); i++) {
-        s[i * 2 + 1] = ss[i];
-    }
-    vector<int> p(size(s), 1);
-    for (int i = 0, l = 0, r = 0; i < size(s); i++) {
-        p[i] = max(min(p[l * 2 - i], r - i), 1);
-        while (0 <= i - p[i] && i + p[i] < size(s) && s[i - p[i]] == s[i + p[i]]) {
-            l = i, r = i + p[i], p[i]++;
-        }
-    }
-    return p;
+vector<int> manacher(string &s) {//pad s with $->$a$b$c....z$, realans = ans-1.
+  string t(size(s) * 2 + 1, '$');
+  for(int i=0;i<size(s);i++) t[i * 2 + 1] = s[i];
+  int n = size(t), m = 0, r = 0;
+  vector<int> p(n);
+  for(int i=0;i<n;i++) {
+    p[i] = (r > i ? min(r - i, p[m - (i - m)]) : 1);
+    while (i-p[i]>=0 && i + p[i] < n &&
+        t[i - p[i]] == t[i + p[i]]) ++p[i];
+    if (i + p[i] > r) m = i, r = i + p[i];
+  }
+  return p;
 }
