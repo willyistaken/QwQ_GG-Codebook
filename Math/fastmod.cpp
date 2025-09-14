b995a64c79c9850ecbdef143a1fb4e105e9a5c32
gaga999 <jjjj4444.h@gmail.com>
2025-09-14 11:58:26 +0800

    fastmod

--- Math/fastmod.cpp
+++ Math/fastmod.cpp
@@ -3,5 +3,7 @@ inline int ad(int a,int b){
 	return a>=M?a-M:a;
 }
 inline int ml(ll a, ll b) {
-    return a * b - (uint64_t)((__uint128_t(-1ULL / M) * a * b) >> 64) * M;
+    int res= a * b - (uint64_t)((__uint128_t(-1ULL / M) * a * b) >> 64) * M;
+	return res>=M?res-M:res;
 }
+
