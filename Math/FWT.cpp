1e855fa2d79134491e3d16dde37afea6c8d0a6fe
willyistaken <willyhlchan@gmail.com>
2025-09-19 09:30:33 +0800

    update bitwise

--- Math/FWT.cpp
+++ Math/FWT.cpp
@@ -19,3 +19,19 @@ inline void FWT(vector<T> &a, int n, int d, int cop, bool inv) {
 [A_0  AND:[1 +-1  OR:[1 0 ]  XOR:0.5/1[1  1
  A_1]      0  1]     [+-1 1]          [1 -1]
 */
+
+template <typename T>
+vector<T> CONV(vector<T> ar, vector<T> br, int cop) {
+    int n = 0;
+    while (1 << n < max(size(ar), size(br)))
+        n++;
+    ar.resize(1 << n), br.resize(1 << n);
+    for (int d = 0; d < n; d++)
+        FWT(ar, n, d, cop, 0), FWT(br, n, d, cop, 0);
+    for (int i = 0; i < 1 << n; i++)
+        ar[i] *= br[i];
+    for (int d = n - 1; d >= 0; d--)
+        FWT(ar, n, d, cop, 1);
+    return ar;
+}
+
