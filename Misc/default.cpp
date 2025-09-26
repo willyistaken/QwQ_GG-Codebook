dd331954a19c12d04cd33101ce311518fb115db1
gaga999 <jjjj4444.h@gmail.com>
2025-09-26 11:22:03 +0800

    defaulttypedef

--- Misc/default.cpp
+++ Misc/default.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define pb emplace_back
 #define iter(x) (x).begin(), (x).end()
 #define size(x) (int)(x).size()
-ypedef long long ll;
+typedef long long ll;
 
 signed main() {
     cin.tie(0)->sync_with_stdio(0);
