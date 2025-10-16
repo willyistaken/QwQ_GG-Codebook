a326bf09b396014847fad8220b392d595c35c2ae
willyistaken <willyhlchan@gmail.com>
2025-10-16 17:32:34 +0800

    update half plane intersection

--- Geometry/HalfPlaneIntersect.cpp
+++ Geometry/HalfPlaneIntersect.cpp
@@ -16,20 +16,32 @@ vector<Line> halfPlaneInter(vector<Line> arr) {
   sort(iter(arr), [&](Line a, Line b) -> int {
     return cmp(a.Y - a.X, b.Y - b.X);
   });
-  deque<Line> dq(1, arr[0]);
-  for (auto p : arr) {
-    if (((dq.back().Y - dq.back().X) ^ (p.Y - p.X)) == 0)
-      continue;
-    while (size(dq) >= 2 && !isin(p, dq[size(dq) - 2], dq.back()))
-      dq.pop_back();
-    while (size(dq) >= 2 && !isin(p, dq[0], dq[1]))
-      dq.pop_front();
+  vector<Line> filtered;
+  for (int i = 0; i < size(arr); i++) {
+    if (i > 0) {
+      auto cross = (arr[i].Y - arr[i].X) ^ (arr[i-1].Y - arr[i-1].X);
+      if (abs(cross) < eps) {
+        if (ori(arr[i-1].X, arr[i-1].Y, arr[i].X) <= 0) {
+          continue;
+        } else {
+          filtered.pop_back();
+        }
+      }
+    }
+    filtered.pb(arr[i]);
+  }
+  if (filtered.empty()) return {};
+  deque<Line> dq(1, filtered[0]);
+  for (int i = 1; i < size(filtered); i++) {
+    auto p = filtered[i];
+    while (size(dq) >= 2 && !isin(p, dq[size(dq) - 2], dq.back())) dq.pop_back();
+    while (size(dq) >= 2 && !isin(p, dq[0], dq[1])) dq.pop_front();
     dq.pb(p);
   }
   while (size(dq) >= 3 && !isin(dq[0], dq[size(dq) - 2], dq.back()))
     dq.pop_back();
   while (size(dq) >= 3 && !isin(dq.back(), dq[0], dq[1]))
     dq.pop_front();
+  if (size(dq) < 3) return {}; 
   return vector<Line>(iter(dq));
 }
-
