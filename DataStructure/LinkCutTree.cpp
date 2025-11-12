a6bdadf7515067927f5ba0640868515638278c55
gaga999 <jjjj4444.h@gmail.com>
2025-11-12 22:52:15 +0800

    lctfix

--- DataStructure/LinkCutTree.cpp
+++ DataStructure/LinkCutTree.cpp
@@ -1,9 +1,18 @@
 struct LCT {
     static const int N = 4e5 + 5; // change
-    int fa[N], ch[2][N], sz[N], sv[N];
+    int fa[N], ch[2][N];
+    ll sz[N], sv[N];
+    bool rtg[N];
 #define gch(x) ((x) == ch[1][fa[x]])
 #define nrt(x) ((x) == ch[gch(x)][fa[x]])
-#define up(x) sz[x] = sz[ch[0][x]] + sz[ch[1][x]] + sv[x] + 1
+#define up(x) sz[x] = sz[ch[0][x]] + sz[ch[1][x]] + sv[x]
+#define rev(x) swap(ch[0][x], ch[1][x]), rtg[x] ^= 1
+#define down(x) rtg[x] && (rev(ch[0][x]), rev(ch[1][x])), rtg[x] = 0
+    void adown(int x) {
+        if (nrt(x))
+            adown(fa[x]);
+        down(x);
+    }
     inline void rota(int x) {
         int f = fa[x], ff = fa[f], k = gch(x);
         if (nrt(f))
@@ -16,6 +25,9 @@ struct LCT {
         up(f), up(x);
     }
     inline void splay(int x) {
+        if (!x)
+            return;
+        adown(x);
         for (int f = fa[x]; nrt(x); rota(x), f = fa[x])
             if (nrt(f))
                 rota(gch(x) ^ gch(f) ? x : f);
@@ -23,30 +35,22 @@ struct LCT {
     inline int acc(int x) {
         int p;
         for (p = 0; x; p = x, x = fa[x])
-            splay(x), sv[x] += sz[ch[1][x]] - sz[p], ch[1][x] = p, up(x);
+            splay(x), ch[1][x] = p, up(x);
         return p;
     }
-    inline int findroot(int x) {
-        int cr = acc(x);
-        while (ch[0][cr])
-            cr = ch[0][cr];
-        splay(cr);
-        return cr;
+    inline void mkrt(int x) {
+        int p = acc(x);
+        rev(p);
     }
-    void link(int x, int y) {
-        int rt = findroot(y);
-        acc(x), acc(rt);
-        acc(y), splay(y);
-        sv[y] += sz[x];
-        fa[x] = y, up(y);
-        acc(rt);
+    inline void link(int x, int y) {
+        mkrt(x), splay(x), fa[x] = y;
     }
-    void cut(int x, int y) {
-        int rt = findroot(y);
-        acc(rt);
-        acc(y), splay(y), splay(x);
-        sv[y] -= sz[x];
-        fa[x] = 0, up(y);
-        acc(x), acc(rt);
+    inline void cut(int x, int y) {
+        mkrt(x), acc(y), splay(y);
+        fa[x] = 0, ch[0][y] = 0, up(y);
+    }
+    inline void ini(int x, int v) {
+        rtg[x] = fa[x] = ch[0][x] = ch[1][x] = 0;
+        sz[x] = sv[x] = v;
     }
 };
