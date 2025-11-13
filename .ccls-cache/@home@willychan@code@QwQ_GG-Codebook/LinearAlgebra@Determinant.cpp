a6efd72e73dda511528b8f2b66f37385d69173ab
willyistaken <willyhlchan@gmail.com>
2025-11-13 21:44:47 +0800

    remove cache

--- .ccls-cache/@home@willychan@code@QwQ_GG-Codebook/LinearAlgebra@Determinant.cpp
+++
@@ -1,9 +0,0 @@
-int Determinant(vector<vector<int>> &a) {
-	assert(size(a) == size(a[0]));
-	GaussElim(a, size(a));
-	int d = 1;
-	for (int i = 0; i < size(a); ++i)
-		d = mmul(d, a[i][i]);
-	return d;
-}
-
